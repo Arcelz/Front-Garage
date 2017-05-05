662f0547c4278767c029ab0cf4addc656f9b42f7
Matheus Neves <matheusnevesdecarvalho@gmail.com>
2017-05-05 15:46:56 -0300

    Add herança no ui-route

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,23 +1,60 @@
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.config( function($httpProvider, $stateProvider, $urlRouterProvider) {
 
 
-    $urlRouterProvider.otherwise('/home');
+     $stateProvider
+      .state('common', {
+        templateUrl: 'app/layout/menu.html',
+        abstract: true,
+      })
+      .state('home', {
+        url: '/',
+        parent: 'common',
+        templateUrl: 'app/views/home.html',
+        controller:'Home',            
+        controllerAs: 'vm'
+        //template: '<div><h4>dashboard</h4></div>',
+        //controller: 'DashboardCtrl'
+      })
+      .state('categoria', {
+        url: '/categorias',
+        parent: 'common',
+        templateUrl: 'app/views/categoria/cadastro-categorias.html',
+        controller:'Categoria',
+        controllerAs: 'vm'
+        //template: '<div><h4>Homeee</h4></div>',
+        //controller: 'DashboardCtrl'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/login.html',
+        controller:'Login',
+        controllerAs: 'vm'
+      });
+
+      $urlRouterProvider.otherwise('/');
+    /*
+
+    $urlRouterProvider.otherwise('/');
 
     $stateProvider
 
         .state('home', {
-            url: '/home',
-            templateUrl: 'app/layout/menu.html',
-            controller:'Home'
+            url: '/',            
+            controller:'Home',            
+            controllerAs: 'vm'
         })
         .state('login', {
-            url: '/login',
-            templateUrl: 'app/login/login.html',
+            url: '/login',            
             controller:'Login'
+        })
+         .state('categoria', {
+            url: '/categorias',
+            templateUrl: 'app/categoria/cadastro-categorias.html',
+            controller:'Categoria'
         })
         .state('home.dashboard', {
             url: '/dashboard',
             templateUrl: 'app/dashboard/dashboard.html',
-        })
+        })*/
 
 });
