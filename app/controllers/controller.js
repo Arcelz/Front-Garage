9d927409157b6d05f9644a7a7c92cc4979f8fbbe
Lucas Goncalves <lucaslucasfabio@hotmail.com>
2017-05-08 14:21:24 -0300

    Mudando o nome de data para DataService
    Mudança de nome de data pra DataService pois alguns erros aconteciam
    pois utulizava-se muito o nome de variavel data, arruma a funcao de get
    e post agora retornam o erro do banco de dados

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,11 +1,11 @@
-app.controller('Login', function ($scope,$location,$state,data,AuthService) {
+app.controller('Login', function ($scope,$location,$state,DataService,AuthService) {
     angular.element(document.querySelector("body")).addClass("login-container login-cover");
     /* data.get('https://jsonplaceholder.typicode.com/posts/').then(function (data) {
      console.log(data.data);
      });*/
     $scope.submit = function () {
         $scope.botaoLogin=true;
-        data.realizarPost('http://ifg.redesbrasil.com/auth/login',$scope.form).then(function (data) {
+        DataService.realizarPost('http://ifg.redesbrasil.com/auth/login',$scope.form).then(function (data) {
             AuthService.setToken(data.data.Token);
             console.log(data.data);
             $scope.botaoLogin=false;
@@ -17,13 +17,13 @@ app.controller('Home', function ($scope,$rootScope) {
     // $rootScope.bodylayout = '';
     angular.element(document.querySelector("body")).removeClass("login-container login-cover");
 });
-app.controller('Funcionario', function ($scope,$rootScope, data) {
+app.controller('Funcionario', function ($scope,$rootScope, DataService) {
     $scope.cargo = null;
     $scope.cargoResultados = [];
 
     selectCargos();
     function selectCargos() {
-        data.get('http://ifg.redesbrasil.com/cargos').then(function (data) {
+        DataService.realizarGet('http://ifg.redesbrasil.com/cargos').then(function (data) {
             $scope.cargoResultados = data.data;
         });
         //$scope.cargoResultados = [ {  "pk_cargos": "1","nome": "ADMIN","status": "ATIVO"}, {"pk_cargos": "3","nome": "matheus","status": "ATIVO"}];
@@ -34,9 +34,8 @@ app.controller('Funcionario', function ($scope,$rootScope, data) {
         //sconsole.log(nome);
         // Chamara função para salvar
         console.log($scope.form2);
-        data.realizarPost('http://ifg.redesbrasil.com/cargos', $scope.form2).then(function (data) {
+        DataService.realizarPost('http://ifg.redesbrasil.com/cargos', $scope.form2).then(function (data) {
             selectCargos();
-
         });
 
         //depois que salvar o cargo no model eu chamo a função;
@@ -49,7 +48,7 @@ app.controller('Funcionario', function ($scope,$rootScope, data) {
         //console.log($scope.vm);
         //$scope.vm = {};
         ///var teste = {"t":$scope.form};
-        data.realizarPost('http://ifg.redesbrasil.com/funcionarios', $scope.form).then(function (data) {
+        DataService.realizarPost('http://ifg.redesbrasil.com/funcionarios', $scope.form).then(function (data) {
             //console.log(data);
             $scope.mensagem = data;
         });
