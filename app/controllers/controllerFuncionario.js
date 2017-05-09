0712369fe3962f7ba336b8520da35e58aee1c69d
MatheusNCarvalho <matheusnevesdecarvalho@gmail.com>
2017-05-08 22:43:18 -0300

    Correção2

--- app/controllers/controllerFuncionario.js
+++ app/controllers/controllerFuncionario.js
@@ -1,4 +1,4 @@
-app.controller('FuncionarioSalvar', function ($scope,$rootScope, data) {
+app.controller('FuncionarioSalvar', function ($scope,$rootScope, DataService) {
     $scope.fkCargo = null;                       
     $scope.cargoResultados = [];
     $scope.titulo = "Funcionario";
@@ -6,7 +6,7 @@ app.controller('FuncionarioSalvar', function ($scope,$rootScope, data) {
     selectCargos();
     //Função para buscar os cargos no WebSerice e coloca no dropdaw
     function selectCargos(){ 
-        data.get('http://ifg.redesbrasil.com/cargos').then(function(data){           // console.log(data);
+        DataService.realizarGet('http://ifg.redesbrasil.com/cargos').then(function(data){           // console.log(data);
            
            $scope.cargoResultados = data.data;
          console.log($scope.cargoResultados,"dentro da Consulta");
@@ -16,7 +16,7 @@ app.controller('FuncionarioSalvar', function ($scope,$rootScope, data) {
     }; 
     //Função salvar cargo 
      $scope.salvarCargo = function () {
-        data.realizarPost('http://ifg.redesbrasil.com/cargos', $scope.form2).then(function () {         
+        DataService.realizarPost('http://ifg.redesbrasil.com/cargos', $scope.form2).then(function () {         
                 selectCargos();           
         });
          //$scope.cargoResultados = [];       
@@ -24,20 +24,58 @@ app.controller('FuncionarioSalvar', function ($scope,$rootScope, data) {
     };
 
     //Função para salvar Funcionario
-    $scope.salvar = function(){
-       
-        data.realizarPost('http://ifg.redesbrasil.com/funcionarios', $scope.form).then(function(data){
-           
-          $scope.mensagem ="Funcionario Salvo com Sucesso";
-          
-        });
-        
-    }   
+    $scope.salvar = function(){       
+     DataService.realizarPost('http://ifg.redesbrasil.com/funcionarios', $scope.form).then(function(response){
+        if(response.data.code == 400){
+              alert(response.data.message);
+        }else{
+             console.log(response,"asdasd");
+        }
+
+
+     }); 
+    }
+});
+
+app.controller('FuncionarioListar', function ($scope,$rootScope, DataService) {
+
+  DataService.realizarGet('http://ifg.redesbrasil.com/funcionarios').then(function(response){
+      if(response.data.length){
+        $scope.lembretes = response.data;
+      }else{
+        $scope.messagem ="Nenhum";
+      }
+
+  });
 
 
 });
 
-app.controller('FuncionarioListar', function ($scope,$rootScope, data) {
+
+app.controller('FuncionarioEditar', function ($scope, DataService,$stateParams) {
+
+    var id = $stateParams.id;
+      console.log(id);
+      DataService.realizarGet('http://ifg.redesbrasil.com/funcionarios/'+id).then(function(response){
+     $scope.form = {
+        nome: response.data[0].nome,
+        cpf: response.data[0].cpf,
+        fkCargo: response.data[0].fk_cargo,
+        email: response.data[0].email,
+
+      };
+     // $scope.fomr = response.data[0];
+      
+      //response.data;
+      console.log($scope.form);
+
+    });
 
 
-});
+  });
+
+
+
+
+
+
