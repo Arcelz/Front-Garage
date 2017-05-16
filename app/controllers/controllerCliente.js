ec90ed49b390dcdd8e5366c1aba7add819ab57a8
inacioedson <inacioedson@hotmail.com>
2017-05-15 22:31:07 -0300

    Criando tela editar cliente
    Criando tela editar cliente

--- app/controllers/controllerCliente.js
+++ app/controllers/controllerCliente.js
@@ -94,3 +94,34 @@ app.controller('ClienteListar', function ($scope,$rootScope, DataService) {
 
 
 });
+
+app.controller('ClienteEditar', function ($scope, DataService,$stateParams,$rootScope) {
+    $scope.form = {};
+    var id = $stateParams.id;
+    var idC;
+    var nome;
+      //console.log(id);
+      DataService.realizarGet('http://ifg.redesbrasil.com/clientes/'+id).then(function(response){
+       // console.log(response,"vindo");
+     $scope.form = {
+       pk_cliente: response.data[0].pk_cliente,
+        nome: response.data[0].nome,
+        cpf: response.data[0].cpf,
+        email: response.data[0].email,
+        logradouro: response.data[0].logradouro,
+        cep: response.data[0].cep,
+        bairro: response.data[0].bairro,
+        cidade: response.data[0].cidade,
+        contato: response.data[0].contato,
+        contato1:response.data[0].contato1,
+        estado:response.data[0].estado,
+        pais:response.data[0].pais,
+
+
+      };
+
+    });
+
+
+
+  });
