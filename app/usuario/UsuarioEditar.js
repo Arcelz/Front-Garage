e06d46cd42e42f9d9b26ca88c47cead6aeeb9289
Lucas Gonçalves de Paula <lucaslucasfabio@hotmail.com>
2017-05-18 14:58:41 -0300

    Alteracoes no select funcionando 100%

--- app/usuario/UsuarioEditar.js
+++ app/usuario/UsuarioEditar.js
@@ -1,54 +1,57 @@
-app.controller('UsuarioEditar', function ($scope,$state, $q, DataService, $stateParams, $rootScope) {
+app.controller('UsuarioEditar', function ($scope, $state, $q, DataService, $stateParams, $rootScope) {
     $scope.form = {};
     var id = $stateParams.id; //pega o paramentro informado na url
     $scope.funcionarioResultados = [];
-    var deferred = $q.defer();
-    var resultadoGrupo;
-    DataService.realizarGet('http://ifg.redesbrasil.com/funcionarios').then(function (data) {
-        deferred.resolve(data);
-    });
-    DataService.realizarGet('http://ifg.redesbrasil.com/grupos').then(function (data) {
-        resultadoGrupo = data.data;
-    });
     $scope.grupoResultados = [];
-    var clicked = true;
-    var clickedGrupo = true;
-    DataService.realizarGet('http://ifg.redesbrasil.com/usuarios/' + id).then(function (response) {
 
-        $scope.form.login = response.data[0].login;
-        $scope.form.senha2 = '**********';
-        $scope.form.senha = '**********';
+    DataService.realizarGet('http://ifg.redesbrasil.com/funcionarios').then(function (data) {
+        $scope.funcionarioResultados = data.data;
+        DataService.realizarGet('http://ifg.redesbrasil.com/grupos').then(function (data2) {
+            $scope.grupoResultados = data2.data;
+            var clicked = true;
+            var clickedGrupo = true;
+            DataService.realizarGet('http://ifg.redesbrasil.com/usuarios/' + id).then(function (response) {
+                $scope.form.login = response.data[0].login;
+                $scope.form.senha2 = '**********';
+                $scope.form.senha = '**********';
+                angular.element('#renderedCombo2').append('<option  value="' + response.data[0].pk_funcionario + '" selected>' + response.data[0].f_nome + '</option>');
+                angular.element('#renderedCombo3').append('<option  value="' + response.data[0].pk_grupo + '" selected>' + response.data[0].g_nome + '</option>');
 
-        angular.element('#renderedCombo2').append('<option  value="' + response.data[0].pk_funcionario + '" selected>' + response.data[0].f_nome + '</option>');
-        angular.element('#renderedCombo3').append('<option  value="' + response.data[0].pk_grupo + '" selected>' + response.data[0].g_nome + '</option>');
 
-        deferred.promise.then(function (resolve) {
-            $scope.loadFuncionarios = function () {
-                if (clicked) {
-                    $scope.funcionarioResultados = resolve.data;
-                    clicked = false;
-                    $("#renderedCombo2 option[value=" + response.data[0].pk_funcionario + "]").remove();
+                $scope.loadGrupos = function () {
+                    if (clickedGrupo) {
+                        clickedGrupo = false;
+                        $(".select2-results__option.select2-results__option--highlighted").remove();
+                        $("#renderedCombo3 option[value=" + response.data[0].pk_grupo + "]").remove();
+                    }
                 }
-            }
-            $scope.loadGrupos = function () {
-                if (clickedGrupo) {
-                    $scope.grupoResultados = resultadoGrupo;
-                    clickedGrupo = false;
-                    $("#renderedCombo3 option[value=" + response.data[0].pk_grupo + "]").remove();
+                $scope.loadFuncionarios = function () {
+                    if (clicked) {
+                        clicked = false;
+                        $(".select2-results__option.select2-results__option--highlighted").remove();
+                        $("#renderedCombo2 option[value=" + response.data[0].pk_funcionario + "]").remove();
+                    }
                 }
-            }
-        }, function (reject) {
-            alert('Erro: ' + reject);
+            });
         });
     });
+
+
     $scope.salvar = function () {
-        $scope.form.funcionario_id = $("#renderedCombo2 option:selected").val();
-        $scope.form.grupo_id = $("#renderedCombo3 option:selected").val();
+        if ($scope.form.funcionario_id === undefined || $scope.form.funcionario_id === "?") {
+            $scope.form.funcionario_id = $("#renderedCombo2 option:selected").val();
+        }
+        if ($scope.form.grupo_id === undefined || $scope.form.grupo_id === "") {
+            $scope.form.grupo_id = $("#renderedCombo3 option:selected").val();
+        }
         console.log($scope.form)
-        DataService.realizarPut('http://ifg.redesbrasil.com/usuarios/' + id, $scope.form).then(function (response) {
-            console.log(response)
-            $scope.botao = false;
-            $state.go('home');
-        })
+        if ($scope.form.funcionario_id != "" || $scope.form.grupo_id != "") {
+            DataService.realizarPut('http://ifg.redesbrasil.com/usuarios/' + id, $scope.form).then(function (response) {
+                console.log(response)
+                $scope.botao = false;
+                $state.go('home');
+            })
+        }
+
     }
-})
+});
