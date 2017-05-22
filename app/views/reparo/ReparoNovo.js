6d3567d002ca8470d80461fec77b57143d61a549
Lucas Gonçalves de Paula <lucaslucasfabio@hotmail.com>
2017-05-22 05:05:02 -0300

    modal de tipo de reparo funcionando
    falatando messagens ao terminar telas e fechar modal apos adicao

--- app/views/reparo/ReparoNovo.js
+++ app/views/reparo/ReparoNovo.js
@@ -8,10 +8,23 @@ app.controller('ReparoNovo', function ($scope, $rootScope, DataService) {
         $scope.veiculosResultados = data.data;
     });
     $scope.salvar = function () {
-            if ($scope.formulario.$valid) {
-                DataService.realizarPost('http://ifg.redesbrasil.com/reparos',$scope.form).then(function (data) {
-                    console.log(data)
-                });
-            }
+        if ($scope.form.fkTipo === undefined || $scope.form.fkTipo === "") {
+            $scope.form.fkTipo = $("#selectTipo option:selected").val();
         }
-})
+        if ($scope.formulario.$valid) {
+            DataService.realizarPost('http://ifg.redesbrasil.com/reparos', $scope.form).then(function (data) {
+                console.log(data)
+            });
+        }
+        console.log($scope.form)
+
+    }
+    $scope.salvarModalTipo = function () {
+        if ($scope.modalFormulario.$valid) {
+            DataService.realizarPost('http://ifg.redesbrasil.com/tipos-reparos', $scope.modal).then(function (data) {
+                console.log(data)
+                angular.element('#selectTipo').append('<option  value="' + data.data.pk_tipo + '" selected>' + data.data.nome + '</option>');
+            });
+        }
+    }
+});
